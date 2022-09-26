f73268b4a118fc83d4953e087cfc0a07ba41b4ca
dogukanakkaya <doguakkaya27@hotmail.com>
2022-09-27 01:00:26 +0300

    refactor: separate routes folder

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
 mod controllers;
+mod routes;
 
 use actix_web::{web, App, HttpServer};
-use controllers::signal_controller::SignalController;
-
-fn signal_config(cfg: &mut web::ServiceConfig) {
-    cfg.service(
-        web::resource("/register/{phone}").route(web::get().to(SignalController::register)),
-    )
-    .service(
-        web::resource("/register/captcha/{phone}")
-            .route(web::get().to(SignalController::register_captcha)),
-    )
-    .service(web::resource("/verify/{phone}").route(web::get().to(SignalController::verify_code)));
-}
+use routes::signal;
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(web::scope("/api").configure(signal_config)))
+    HttpServer::new(|| App::new().service(web::scope("/api").configure(signal::config)))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
---
+++ src/routes/signal.rs
@@ -0,0 +1,13 @@
+use crate::controllers::signal_controller::SignalController;
+use actix_web::web;
+
+pub fn config(cfg: &mut web::ServiceConfig) {
+    cfg.service(
+        web::resource("/register/{phone}").route(web::get().to(SignalController::register)),
+    )
+    .service(
+        web::resource("/register/captcha/{phone}")
+            .route(web::get().to(SignalController::register_captcha)),
+    )
+    .service(web::resource("/verify/{phone}").route(web::get().to(SignalController::verify_code)));
+}
